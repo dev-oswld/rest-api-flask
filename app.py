b95a689fc7c0ff0a682f6e384abdee9b8243d8e9
Oswld <user.oswld@gmail.com>
2020-06-09 17:59:58 -0500

    url static and dynamic

--- app.py
+++ app.py
@@ -10,8 +10,22 @@ def index():
 # Extra
 @app.route("/about")
 def about():
-    return "Rest API with Flask (micro-framework)"
+    return "RESTful API with Flask (micro framework)"
+
+# Working with string and int
+@app.route("/user/<int:id>/<string:user>")
+def username(id, user):
+    return "Hello {} the ID is {}".format(user, id)
+
+# And floats 
+@app.route("/sum/<float:a>/<float:b>")
+@app.route("/sum/")
+def sum(a=1, b=2):
+    if a is 1:
+        return "The default sum is {}".format(a + b)
+    else:
+        return "The dynamic sum is {}".format(a + b)
 
 if __name__ == "__main__":
-    app.run()
-
+    # Auto reloading
+    app.run(host='127.0.0.1', port=2000, debug=True)
