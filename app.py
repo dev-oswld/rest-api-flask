5ab92e3ab72006c1f0fc0c55e21053a64265bc2a
Oswld <user.oswld@gmail.com>
2020-06-09 18:09:52 -0500

    Implement render template method

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Decorator route
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 # Extra
 @app.route("/about")
