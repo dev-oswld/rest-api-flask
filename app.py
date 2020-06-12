9a86c15835cef162a9ab1d6bbdebe29bdc641a33
Oswld <user.oswld@gmail.com>
2020-06-12 16:12:27 -0500

    Database schema, connection and posts

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
+# Class for DB
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    
 # Decorator route
 @app.route("/")
 def index():
@@ -9,25 +20,26 @@ def index():
     list = ["i", "love", "python"]
     return render_template("index.html", title=title, list=list)
 
+# Insert method
+@app.route("/insert/default")
+def insert_default():
+    new_post = Posts(title="Default title")
+    db.session.add(new_post)
+    db.session.commit()
+    return "The post was created"
+
+# Select method
+@app.route("/select/default")
+def select_default():
+    post = Posts.query.filter_by(id=1).first()
+    print(post.title)
+    return "Query done, look the console"
+
 # Extra
 @app.route("/about")
 def about():
     return "RESTful API with Flask (micro framework)"
 
-# Working with string and int
-@app.route("/user/<int:id>/<string:user>")
-def username(id, user):
-    return "Hello {} the ID is {}".format(user, id)
-
-# And floats 
-@app.route("/sum/<float:a>/<float:b>")
-@app.route("/sum/")
-def sum(a=1, b=2):
-    if a is 1:
-        return "The default sum is {}".format(a + b)
-    else:
-        return "The dynamic sum is {}".format(a + b)
-
 if __name__ == "__main__":
-    # Auto reloading
+    db.create_all()
     app.run(host='127.0.0.1', port=2000, debug=True)
