ef3d6c791d166605aa2426eeef820df9a1d79345
Oswld <user.oswld@gmail.com>
2020-07-01 19:03:29 -0500

    mmmm, to-fix

--- _examples/alternative_app.py
+++ _examples/alternative_app.py
@@ -4,47 +4,87 @@ Show in console/browser (localhost) the request.
 '''
 
 from flask import Flask, jsonify
-
-# Example JSON
-names = [
-    {
-        "id": 1,
-        "title": "oswaldo"
-    },
-    {
-        "id": 2,
-        "title": "oswald"
-    },
-    {
-        "id": 3,
-        "title": "oswld"
-    }
-]
-
-scripts = [
-    {
-        "id": 1,
-        "title": "python"
-    },
-    {
-        "id": 2,
-        "title": "javascript"
-    },
-    {
-        "id": 3,
-        "title": "perl"
-    }
-]
+import requests
 
 app = Flask(__name__)
 request_status = ""
+
+# REST API public
+host = "https://jsonplaceholder.typicode.com"
+
+# GET
+@app.route("/get-posts", methods=["GET"])
+def get_posts():
+    r = requests.get(host + "/posts")
+    for p in r.json():
+        post = "userId: {}\nid: {}\nTitle: {}\nPost: {}\n\n".format(
+            p["userId"], p["id"], p["title"], p["body"])
+        print(post)
+    
+    # All post
+    return "Yeah, it´s works"
+
+
+# POST
+@app.route("/add-post", methods=["POST"])
+def add_post():
+    userId = 11
+    title = "Esto es en español"
+    body = "Más texto escrito en español para variar el contenido"
+
+    # See more => https://en.wikipedia.org/wiki/Payload_(computing)
+    # The part of transmitted data, related to headers and metadata
+    payload = {"userId": userId, "title": title, "body": body}
+    r = requests.post(host + "/posts", json=payload)
+
+    p = r.json()
+    post = "userId: {}\nid: {}\nTitle: {}\nPost: {}\n\n".format(
+        p["userId"], p["id"], p["title"], p["body"])
+    print("\nNew post")
+    print(post)
+
+    return "Yeah, yeah"
+
+
+# PUT
+@app.route("/edit-post", methods=["PUT"])
+def edit_post():
+    id = 100
+    userId = 10
+    title = "Tengo más contenido en español por hacer"
+    body = "Aquí debe de haber mucho texto en español"
+
+    payload = {"userId": userId, "title": title, "body": body}
+    r = requests.put(host + "/posts/" + id, json=payload)
+
+    p = r.json()
+    post = "userId: {}\nid: {}\nTitle: {}\nPost: {}\n\n".format(
+        p["userId"], p["id"], p["title"], p["body"])
+
+    print("\nHere is the post")
+    print(post)
+
+    return "Ok ok"
+
+
+# DELETE
+@app.route("/delete-post", methods=["DELETE"])
+def delete_post():
+    id = 100
+
+    r = requests.delete(host + "/posts/" + id)
+
+    print(r.json())
+    print("The post was deleted")
+
+    return "okey"
+
 
 # Star here
 @app.route("/")
 def index():
     return "Request status ➡️ " + request_status
 
-# Global keyword that allows a user to modify a variable outside of the current scope. 
 # 1
 @app.before_request
 def before_request():
@@ -72,8 +112,7 @@ def teardown_request(response):
 
 @app.route("/api/info")
 def show_info():
-    # key/value
-    return jsonify({"info": {"names": names, "scripts": scripts}})
+    return "REST API"
 
 if __name__ == "__main__":
     app.run(port=2020, debug=True)
